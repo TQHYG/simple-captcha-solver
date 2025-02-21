3bdef37be5088ddcac9506f382c54265f3f480b7
TQHYG <zzh18857376674@outlook.com>
2025-02-21 13:51:45 +0800

    modified:   captcha_solver.py 	modified:   main.py

--- captcha_solver.py
+++ captcha_solver.py
@@ -1,10 +1,21 @@
+import os
+import sys
 import ddddocr
 from PIL import Image
 import numpy as np
 
 def solve_captcha(image_path):
     try:
+        # 保存原stdout
+        original_stdout = sys.stdout
+        # 打开os.devnull作为新的stdout
+        sys.stdout = open(os.devnull, 'w')
+
+        # 初始化DdddOcr对象
         ocr = ddddocr.DdddOcr()
+
+        # 恢复原stdout
+        sys.stdout = original_stdout
         # 打开图片
         with open(image_path, 'rb') as image:
             img_bytes = image.read()
